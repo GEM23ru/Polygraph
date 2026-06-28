cdb27a7ce8cd6f3918d72d75f7ff1b2263aecd34
GEM23ru <167005857+GEM23ru@users.noreply.github.com>
2026-06-28 09:21:45 +0300

    Delete Полиграф история directory

--- Полиграф история/Остановить Polygraph.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Polygraph - stop
-echo.
-echo   Ostanavlivayu Polygraph...
-taskkill /F /IM pythonw.exe >nul 2>&1
-taskkill /F /IM python.exe >nul 2>&1
-echo   Gotovo. Server ostanovlen.
-echo.
-timeout /t 2 >nul
